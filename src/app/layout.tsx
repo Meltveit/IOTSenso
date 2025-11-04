989e5b8e1c0a61670224dc9e09c2d2cef01271e5
Christopher Nielsen <Kontakt@fm-service.no>
2025-11-04 22:06:17 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
           "min-h-screen bg-background font-body antialiased",
           inter.variable
         )}
+        suppressHydrationWarning={true}
       >
         <AuthProvider>
             {children}
